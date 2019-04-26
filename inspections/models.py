ca57a42bcf5740d6a8e572307ef3e8163e040576
aidianholder <aidianholder@gmail.com>
2019-04-26 08:45:31 -0700

    models done-ish

--- inspections/models.py
+++ inspections/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import uuid
+from decimal import Decimal
 
 
 class Facility(models.Model):
@@ -10,14 +11,36 @@ class Facility(models.Model):
     facility_uuid = models.UUIDField(unique=True)
 
     def __str__(self):
-        return "%s %s" (self.facility_name, self.facility_city)
+        return "{0}{1}".format(self.facility_name, self.facility_city)
+
+    def pass_fail_total(self):
+        all_inspections = self.inspection_set.all()
+        passed = Decimal(0)
+        failed = Decimal(0)
+        for event in all_inspections:
+            if event.passed_or_failed() == 'passed':
+                passed += 1
+            if event.passed_or_failed() == 'failed':
+                failed += 1
+        failed_percent = 100 * (Decimal(failed) / Decimal(failed + passed))
+        passed_percent = Decimal(100) - failed_percent
+        return {'failed': failed, 'passed': passed, 'failed_percent': failed_percent, 'passed_percent': passed_percent}
+
+    def latest_inspection(self):
+        most_recent = self.inspection_set.latest('inspection_date')
+        return most_recent.inspection_date
+
+    def assign_uuid(self):
+        if not self.facility_uuid:
+            working_str = str("{0}{1}".format(self.facility_name.strip(), self.facility_address.strip()))
+            self.facility_uuid = uuid.uuid3(uuid.NAMESPACE_DNS, working_str)
 
     class Meta:
         verbose_name_plural = "Facilities"
 
 
 class Inspection(models.Model):
-    facility = models.ForeignKey(Facility, to_field="facility_uuid", on_delete="cascade")
+    facility = models.ForeignKey(Facility, to_field="facility_uuid", on_delete=models.CASCADE)
     inspect_date = models.DateField()
     inspect_type = models.CharField(max_length=40, blank=True)
     inspect_result = models.CharField(max_length=20, blank=True)
@@ -25,5 +48,19 @@ class Inspection(models.Model):
     non_critical = models.IntegerField()
     total_points = models.IntegerField()
     inspect_details = models.TextField(blank=True)
+
+    def __str__(self):
+        return "{0}{1}{2}".format(self.facility.facility_name, self.inspect_type, self.inspect_type)
+
+    def passed_or_failed(self):
+        result_cleaned = self.inspect_result.strip().lower()
+        if result_cleaned == "failed" or result_cleaned == "re-inspection required" or result_cleaned == "compliance required":
+            return 'failed'
+        else:
+            return 'passed'
+
+    class Meta:
+        ordering = ['-inspect_date']
+        verbose_name_plural = "Inspections"
 
 
--- inspections/views.py
+++ inspections/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.core import serializers
+import json
 
-# Create your views here.
+from .models import Facility, Inspection
+
+
+def index(request):
+    return HttpResponse("index page goes here.  build an index page aidian")
+
+
+def facility_by_name(request, name_string):
+    name_string_clean = name_string.upper().strip()
+    facilities = Facility.objects.filter(facilty_name__startswith=name_string_clean)
+
+
+
+
+
+
+
